ab3d9733386a26f591cb390b766d5bb63aef2d19
Lucas Cherkewski <lucas@lucascherkewski.com>
2022-11-26 14:48:59 -0500

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function(eleventyConfig) {
+  // Return your Object options:
+  return {
+    dir: {
+      input: "src",
+	  includes: "includes",
+	  layouts: "layouts",
+	  data: "data",
+    }
+  }
+};
