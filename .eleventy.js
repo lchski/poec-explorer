d65abd6fa018c8c4f591acd8f2cdbf01d6764890
Lucas Cherkewski <lucas@lucascherkewski.com>
2022-11-26 19:31:14 -0500

    add support for CSV
    via: https://www.maxkohler.com/posts/eleventy-csv/

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,21 @@
+const parse = require("csv-parse/lib/sync");
+
 module.exports = function(eleventyConfig) {
-  // Return your Object options:
-  return {
-    dir: {
-      input: "src",
-	  includes: "includes",
-	  layouts: "layouts",
-	  data: "data",
-    }
-  }
+	eleventyConfig.addDataExtension("csv", (contents) => {
+		const records = parse(contents, {
+			columns: true,
+			skip_empty_lines: true,
+		});
+		return records;
+	});
+	
+	// Return your Object options:
+	return {
+		dir: {
+			input: "src",
+			includes: "includes",
+			layouts: "layouts",
+			data: "data",
+		}
+	}
 };
