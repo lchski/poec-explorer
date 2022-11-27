c5c0cfe09871a6fb2653ba90d44fbe488a763328
Lucas Cherkewski <lucas@lucascherkewski.com>
2022-11-26 19:36:05 -0500

    change source directories to _ prefix

--- .eleventy.js
+++ .eleventy.js
@@ -13,9 +13,9 @@ module.exports = function(eleventyConfig) {
 	return {
 		dir: {
 			input: "src",
-			includes: "includes",
-			layouts: "layouts",
-			data: "data",
+			includes: "_includes",
+			layouts: "_layouts",
+			data: "_data",
 		}
 	}
 };
